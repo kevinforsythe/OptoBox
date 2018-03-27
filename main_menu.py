056625cb4d6d822a738f71cd3e3d75ea444c9ebc
Kevin Forsythe <kevin.forsythe@gmail.com>
2018-03-27 15:35:49 -0700

    activated upload old song option

--- main_menu.py
+++ main_menu.py
@@ -2,11 +2,11 @@ import os
 
 def main_menu():
   print("\n\t\t\tMAIN MENU OPTIONS:")
-  print("\n\t1. Upload a Previously Written Song to OptoBox")
+  print("\n\t1. Upload an Old (i.e. Previously Transposed) Song to OptoBox")
   print("\n\t2. Create a New Song from a *.csv File and Upload to OptoBox")
-  print("\t   (currently works only for single column/channel LED control)")
+  print("\t   (currently only works for single column/channel LED control)")
   print("\n\t3. See a List of Previously Written Songs")
-  print("\t    (Press 'q' to return to the main menu)")
+  print("\t    (NOTE: Press 'q' to return to the main menu)")
   print("\n\t4. See a Log of Songs Previously Uploaded to OptoBox")
   print("\n\t5. Quit")
   main_menu_choice = input("\n\nWhat would you like to do?\n>")
@@ -22,18 +22,17 @@ def main_menu():
       quit()
   else:
       os.system('clear')
-      print("\n\n\n\n\nPlease Choose a Valid Option:\n")
+      print("\nPlease Choose a Valid Option:\n")
       main_menu()
 
 
 def main_menu_1():
     os.system('clear')
-    print("\nsorry, option1 not yet implemented\n\n")
+    os.system('python3 upload_song.py')
     main_menu()
 
 def main_menu_2():
     os.system('python3 transpose_song.py')
-    # call compile&upload function here
     main_menu()
 
 def main_menu_3():
@@ -50,5 +49,5 @@ def main_menu_4():
 
 # this is the main start
 os.system('clear')
-print("\n\n\nWelcome to OptoBoxComposer (version alpha, for single 35mm dish)")
+print("\nWelcome to OptoBoxComposer (version alpha, for single 35mm dish only)")
 main_menu()
