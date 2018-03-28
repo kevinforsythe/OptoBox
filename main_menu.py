85298d5d1c49bd7fc3a8792a2b5848b424c44475
Kevin Forsythe <kevin.forsythe@gmail.com>
2018-03-28 10:47:59 -0700

    added option to launch spread sheet app

--- main_menu.py
+++ main_menu.py
@@ -2,15 +2,19 @@ import os
 
 def main_menu():
   print("\n\t\t\tMAIN MENU OPTIONS:")
-  print("\n\t1. Upload an Old (i.e. Previously Transposed) Song to OptoBox")
-  print("\n\t2. Create a New Song from a *.csv File and Upload to OptoBox")
+  print('\n\t0. Open Spreadsheet Program to Create *.csv Data File')
+  print("\t(Remember to 'Save As' .csv Format)")
+  print("\n\t1. Create a New Song from a *.csv File and Upload to OptoBox")
   print("\t   (currently only works for single column/channel LED control)")
-  print("\n\t3. See a List of Previously Written Songs")
-  print("\t    (NOTE: Press 'q' to return to the main menu)")
+  print("\n\t2. See a List of Previously Transposed Songs")
+  print("\n\t3. Upload a Previously Transposed Song to OptoBox")
+  print("\t    (NOTE: After viewing, press 'q' to return to the main menu)")
   print("\n\t4. See a Log of Songs Previously Uploaded to OptoBox")
   print("\n\t5. Quit")
   main_menu_choice = input("\n\nWhat would you like to do?\n>")
-  if main_menu_choice == '1':
+  if main_menu_choice == '0':
+      main_menu_0()
+  elif main_menu_choice == '1':
       main_menu_1()
   elif main_menu_choice == '2':
       main_menu_2()
@@ -25,18 +29,23 @@ def main_menu():
       print("\nPlease Choose a Valid Option:\n")
       main_menu()
 
+def main_menu0():
+    to_open_libre_calc= "libreoffice --calc"
+    os.system(to_open_libre_calc)
+    os.system('clear')
+    print('--remember to save your data in CSV format!')
 
 def main_menu_1():
     os.system('clear')
-    os.system('python3 upload_song.py')
-    main_menu()
-
-def main_menu_2():
     os.system('python3 transpose_song.py')
     main_menu()
 
-def main_menu_3():
+def main_menu_2():
     os.system("ls -lu --time-style=long-iso ./output_songs | awk '{print $6, $7, $8, $9}' | less")
+    main_menu()
+
+def main_menu_3():
+    os.system('python3 upload_song.py')
     os.system('clear')
     main_menu()
 
