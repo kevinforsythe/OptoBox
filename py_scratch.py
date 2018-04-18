85807be13ae0747e7aaef6abcd445a6c6df3aba4
Kevin Forsythe <kevin.forsythe@gmail.com>
2018-04-18 08:56:56 -0700

    working on new input function

--- py_scratch.py
+++ py_scratch.py
@@ -23,7 +23,8 @@ def compose_song():
 
         # input PWM settings for when LED is on
     print("\nWhen your LEDs are active, do you want to modulate their power with Pulse Width Modification (PWM)?")
-    print("(ie, do you want to attenuate LED output by decreasing LED duty cycle when it's on?)")
+    print("ie, do you want to attenuate LED output by decreasing LED duty cycle when it's on?")
+    print("(by entering 'n', the LEDs will illuminate with full strength (100%) when on)")
     PWM_option = input("y/n)\n>")
     if PWM_option == 'y':
         PWM_option = 1
@@ -41,9 +42,9 @@ def compose_song():
         else:
             PWM_option = 999
     if PWM_option == 1:
-        LED_duty_cycle = int(input("\n\tplease enter the desired percent duty cycle (1-100):"))
-    # trying to restrict input to valid response...
-    #    is_LED_duty_cycle_int = isinstance(LED_duty_cycle, int)
+        LED_duty_cycle = int(input("\n\tplease enter the desired percent duty cycle (1-100, omit the %%' sign):"))
+    # ToDo: restrict input to valid response...
+    # is_LED_duty_cycle_int = isinstance(LED_duty_cycle, int)
         while (LED_duty_cycle > 101) | (LED_duty_cycle < 0):
             LED_duty_cycle = int(input("\n\tplease enter the desired percent duty cycle (1-100):"))
         print("OK, LED duty cycle will be %d%%" % (LED_duty_cycle))
@@ -73,17 +74,19 @@ def compose_song():
 
         #input LED Active/Resting times and total number of cycle repeats
     print("\nPlease provide parameters for the 'LED Active/Resting cycle':")
-    LED_active_time = float(input("\tfor each 'LED Active/Resting cycle', how long should the LEDs be 'Active' (in seconds)?")
-    LED_rest_time = float(input("\tbetween each 'Active' period, how long should the LEDs be 'Resting' (ie turned off, in seconds)?"))
+    print("\tfor each 'LED Active/Resting cycle', how many seconds should the LEDs be 'Active'?")
+    LED_active_time = float(input("\t>"))
+    print("\tbetween each 'Active' period, how many seconds should the LEDs be 'Resting'?")
+    LED_rest_time = float(input("\t>"))
     print("OK, for every 'LED Active/Resting cycle', the LEDs will be active for %d seconds, then rest for %d seconds," % (LED_active_time, LED_rest_time))
-    sum_LED_active_resting_times = LED_active_time + LED_rest_time
-    print("so each 'LED Active/Resting cycle' will last %d seconds." % (sum_LED_active_resting_times))
+    sum_LED_active_and_rest_time = LED_active_time + LED_rest_time
+    print("so each 'LED Active/Resting cycle' will last   %d seconds." % (sum_LED_active_and_rest_time))
     print("\nHow many times would you like to repeat the 'LED Active/Resting cycle'?")
     num_LED_pattern_repeats = int(input("please enter a number:"))
-    print("OK, your LEDs will repeat the 'Active/Resting' cycle a %d times." % (num_LED_pattern_repeats))
-    print("so your total song time will be %d seconds." % (sum_LED_active_resting_times))
+    print("OK, your LEDs will repeat the 'Active/Resting' cycle %d times," % (num_LED_pattern_repeats))
+    print("so your song's total time length will be   %d seconds." % (num_LED_pattern_repeats*sum_LED_active_and_rest_time))
 
-    num_blinks_per_active_phase = LED_active_time/blink_period
+    num_blinks_per_active_phase = int(LED_active_time/blink_period)
 
 
     # recall, path_new_song_dir = "~/Documents/Optobox_Files/Optobox_Songs/"+(new_song_name)
@@ -91,12 +94,42 @@ def compose_song():
     path_temp_song_parameters = path_new_song_dir+"/."+(new_song_name)+".txt"
     handle_song_parameters_txt = open(path_temp_song_parameters, 'w')
 
-    # Set the number of 'LED Active/Resting' cycle repeats
-    handle_song_parameters_txt.write('for (int i = 0; i < '+ num_LED_pattern_repeats + '; i++) {\n')
 
-    # LEDs Active Period
+#  4 different sections here for a 2x2 matrix of options: +/-PWM and +/- blinking
+    if PWM_option == 0:
+        if blink_frequency == 0:
+            # Set the number of 'LED Active/Resting' cycle repeats
+            handle_song_parameters_txt.write('for (int i = 0; i < '+ num_LED_pattern_repeats + '; i++) {/\n')
+            handle_song_parameters_txt.write('        digitalWrite(LEDBlue, HIGH);\n')
+            handle_song_parameters_txt.write('        delay(' + LED_active_time + ');\n')
+            handle_song_parameters_txt.write('        digitalWrite(LEDBlue, LOW);\n')
+            handle_song_parameters_txt.write('        delay(' + LED_rest_time+ ');\n')
+            handle_song_parameters_txt.write('}')
 
-# ? should i write 2 different sections, 1 for blinking, 1 for 100% duty cycle?
+        if blink frequency != 0:
+            handle_song_parameters_txt.write('for (int i = 0; i < '+ num_LED_pattern_repeats + '; i++) {/\n')
+            handle_song_parameters_txt.write('    for (j = 0; j < '+ num_blinks_per_active_phase + '; j++) {\n')
+            handle_song_parameters_txt.write('    }')
+            handle_song_parameters_txt.write('}')
+
+
+        else:
+            print("error!")
+            quit
+    if PWM_option == 1:
+        if blink_frequency == 0:
+            asdf
+        if blink frequency != 0:
+            asdf
+        else:
+            print("error!")
+            quit
+
+
+
+
+
+
 
     handle_song_parameters_txt.write('    for (j = 0; j < '+ num_blinks_per_active_phase + '; j++) {\n')
     handle_song_parameters_txt.write('        digitalWrite(LEDBlue, HIGH);\n')
@@ -112,36 +145,36 @@ def compose_song():
     # close the for loop for Active/Resting cycle repeats
     handle_song_parameters_txt.write('}')
 
-
-
-
-        handle_song_parameters_txt.write('digitalWrite(LEDBlue, LOW);\n')
-        handle_song_parameters_txt.write('delay(' + beat_value + ');\n')
-
-
-
-        elif song_csv_data[i] == '1\n':
-            handle_song_parameters_txt.write('digitalWrite(LEDBlue, HIGH);\n')
-            handle_song_parameters_txt.write('delay(' + beat_value + ');\n')
-        else:
-            print("ERROR:  You're CSV data is dirty (i.e. contains data other than '1' or '0')")
-            quit()
-    handle_song_parameters_txt.close()
-
-    # now take hidden intermediate txt file and add template text to make ino file
-    song_beginning = open('./.template_files/template_song_intro.txt', 'r')
-    handle_song_parameters_txt = open(path_temp_song_parameters, 'r')
-    song_end = open('./.template_files/template_song_coda.txt', 'r')
-    song_complete = open(path_new_song_dir+"/"+(new_song_name)+".ino", 'w')
-    a = song_beginning.read()
-    b = handle_song_parameters_txt.read()
-    c = song_end.read()
-    song_complete.write(a + b + c)
-    # now close all the files
-    song_beginning.close()
-    handle_song_parameters_txt.close()
-    song_end.close()
-    song_complete.close()
+#
+#
+#
+#         handle_song_parameters_txt.write('digitalWrite(LEDBlue, LOW);\n')
+#         handle_song_parameters_txt.write('delay(' + beat_value + ');\n')
+#
+#
+#
+#         elif song_csv_data[i] == '1\n':
+#             handle_song_parameters_txt.write('digitalWrite(LEDBlue, HIGH);\n')
+#             handle_song_parameters_txt.write('delay(' + beat_value + ');\n')
+#         else:
+#             print("ERROR:  You're CSV data is dirty (i.e. contains data other than '1' or '0')")
+#             quit()
+#     handle_song_parameters_txt.close()
+#
+#     # now take hidden intermediate txt file and add template text to make ino file
+#     song_beginning = open('./.template_files/template_song_intro.txt', 'r')
+#     handle_song_parameters_txt = open(path_temp_song_parameters, 'r')
+#     song_end = open('./.template_files/template_song_coda.txt', 'r')
+#     song_complete = open(path_new_song_dir+"/"+(new_song_name)+".ino", 'w')
+#     a = song_beginning.read()
+#     b = handle_song_parameters_txt.read()
+#     c = song_end.read()
+#     song_complete.write(a + b + c)
+#     # now close all the files
+#     song_beginning.close()
+#     handle_song_parameters_txt.close()
+#     song_end.close()
+#     song_complete.close()
 
 
 
